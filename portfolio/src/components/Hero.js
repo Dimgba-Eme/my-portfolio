8c1da776a5123c1d5a601f16c1d2c7036e757f6f
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-23 22:46:20 +0100

    Styled and aligned the Hero section. Design is also responsive in all views

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -8,11 +8,14 @@ const Hero = () => {
         <section className='hero-container'>
             <div className='hero-inner'>
                 <img src={Profile_img} alt='profile pic' className='profile-img' />
+                <h3 className='profile-name'>Hello, I'm Dimgba!</h3>
 
                 <div className='location-container'>
                     <img src={location_icon} alt='location icon' className='location-icon' />
-                    <p className='location-title'>Abia, Nigeria</p>
+                    <p className='location-title'>Abia, Nigeria.</p>
                 </div>
+
+                <h1 className='intro'>A Full-Stack developer specialized in React for Front-End & Python, Django and MySQL for Back-End development, <span className='expertise'>with more expertise in Front-End!</span></h1>
             </div>
         </section>
     )
