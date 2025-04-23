b39aff6ded8592879f5b181ff5889e63bd7c4f51
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-23 17:26:17 +0100

    Properly aligned the Header contents in mobile view

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -3,11 +3,11 @@ import './header.scss'
 import Navbar from './Navbar'
 
 const Header = () => {
-  return (
-    <header className='header-container'>
-      <Navbar />
-    </header>
-  )
+    return (
+        <header className='header-container'>
+            <Navbar />
+        </header>
+    )
 }
 
 export default Header
