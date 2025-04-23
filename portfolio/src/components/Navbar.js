a5f85ba9c3f6f5ddce7042c7ea9ed1fa358a3df3
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-23 16:29:26 +0100

    Imported the burger menu into the Navbar component for mobile view

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './navbar.scss'
+import burger_menu from '../assets/icons/burger-menu.png'
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section className='navbar-container'>
+            <div className='navbar-inner'>
+                <div>
+                   <img src={burger_menu} alt='burger menu' className='burger-menu' />
+                </div>
+
+            </div>
+        </section>
+    )
 }
 
 export default Navbar
