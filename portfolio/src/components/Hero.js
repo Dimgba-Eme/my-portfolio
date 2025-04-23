d07836aceac2fe08506152213605c1735f87cb07
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-23 20:26:09 +0100

    Created and imported hero.scss file into the Hero component for styling

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import './hero.scss'
+import Profile_img from '../assets/images/profile-img.jpg'
 
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='hero-container'>
+     <div className='hero-inner'>
+       <img src={Profile_img} alt='profile pic' className='profile-img' />
+     </div>
+    </section>
   )
 }
 
