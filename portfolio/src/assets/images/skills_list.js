12b4245622a882b362cf2e224a09b9d034934a30
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-23 23:18:52 +0100

    Imported all the language icons in the skills_list file

---
+++ portfolio/src/assets/images/skills_list.js
@@ -0,0 +1,53 @@
+import html from '../icons/html-5.png'
+import css from '../icons/css-3.png'
+import js from '../icons/js.png'
+import sass from '../icons/sass.png'
+import react from '../icons/react.svg'
+import python from '../icons/python.png'
+import mySql from '../icons/mysql.png'
+import django from '../icons/django.png'
+
+
+const skills_list = [
+    {
+        icon: html,
+        name: 'HTML',
+    },
+
+    {
+        icon, css,
+        name: 'CSS',
+    },
+
+    {
+        icon, js,
+        name: 'JAVASCRIPT',
+    },
+
+    {
+        icon, sass,
+        name: 'SASS',
+    },
+
+    {
+        icon, react,
+        name: 'REACT',
+    },
+
+    {
+        icon, python,
+        name: 'PYTHON',
+    },
+
+    {
+        icon, mySql,
+        name: 'MYSQL',
+    },
+
+    {
+        icon, django,
+        name: 'DJANGO',
+    },
+]
+
+export default skills_list
