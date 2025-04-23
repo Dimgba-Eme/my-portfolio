d3698f0825e6bd778a23aba5d394661d1fefa274
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-23 20:59:03 +0100

    Imported the location icon and imported it into the Hero section

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './hero.scss'
 import Profile_img from '../assets/images/profile-img.jpg'
+import location_icon from '../assets/icons/location.png'
 
 const Hero = () => {
-  return (
-    <section className='hero-container'>
-     <div className='hero-inner'>
-       <img src={Profile_img} alt='profile pic' className='profile-img' />
-     </div>
-    </section>
-  )
+    return (
+        <section className='hero-container'>
+            <div className='hero-inner'>
+                <img src={Profile_img} alt='profile pic' className='profile-img' />
+
+                <div className='location-container'>
+                    <img src={location_icon} alt='location icon' className='location-icon' />
+                    <p className='location-title'>Abia, Nigeria</p>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Hero
