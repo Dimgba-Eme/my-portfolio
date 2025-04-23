f37cfb4d2f5aa4c311b814c8e650acaaf67cd96c
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-23 19:29:59 +0100

    Styled and aligned the Header and Navbar. Also perfectly responsive in all views

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -7,11 +7,12 @@ import close_icon from '../assets/icons/icon-close.svg'
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [linkNav, setLinkNav] = useState("")
     return (
         <section className='navbar-container'>
             <div className='navbar-inner'>
                 <div>
-                    <img src={burger_menu} alt='burger menu' className='burger-menu' />
+                    <img onClick={() => setIsOpen(!isOpen)} src={burger_menu} alt='burger menu' className='burger-menu' />
                 </div>
 
                 <div className='github-linkedin-container'>
@@ -19,17 +20,17 @@ const Navbar = () => {
                     <a href='https://www.linkedin.com/in/dimgba-eme-4616a6111/' target='_blank'><img src={linkedin_icon} alt='linkedin icon' className='linkedin-icon' /></a>
                 </div>
 
-                <div className='nav-links-container'>
-                    <nav>
-                        <img src={close_icon} alt='close icon' className='close-icon' />
+                <nav className={isOpen ? "isOpen" : ""} onClick={() => setIsOpen(!isOpen)}>
+                    <div className='nav-links-container'>
+                        <img onClick={() => setIsOpen(!isOpen)} src={close_icon} alt='close icon' className='close-icon' />
                         <ul className='nav-links-inner'>
-                            <a href='#skills' className='nav-link'>Skills</a>
-                            <a href='#projects' className='nav-link'>Projects</a>
-                            <a href='#contact' className='nav-link'>Contact Me</a>
+                            <a href='#skills' onClick={() => setLinkNav("skills")} className={linkNav === "skills" ? "active" : ""}>Skills</a>
+                            <a href='#projects' onClick={() => setLinkNav("projects")} className={linkNav === "projects" ? "active" : ""}>Projects</a>
+                            <a href='#contact-me' onClick={() => setLinkNav("contact-me")} className={linkNav === "contact-me" ? "active" : ""}>Contact Me</a>
 
                         </ul>
-                    </nav>
-                </div>
+                    </div>
+                </nav>
 
             </div>
         </section>
