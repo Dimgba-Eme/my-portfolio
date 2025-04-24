8c8c8b6b1530eae55d1f5085df6c5688c16f747a
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-24 14:18:18 +0100

    Setup the skills_list, imported and added all the necessary data

--- portfolio/src/assets/images/projects_list.js
+++ portfolio/src/assets/images/projects_list.js
@@ -1,11 +1,22 @@
+import dimmy_store from '../images/Dimmy E-commerce Store.png'
+import eattyfilly from '../images/EattyFilly Restaurant.png'
+
 
 const projects_list = [
     {
-
+        image: dimmy_store,
+        name: "Dimmy's Store",
+        description: 'E-commerce website where customers purchase varieties of items which are delivered to them within a short period of time.',
+        language_one: "REACT",
+        language_two: "SASS",
     },
 
     {
-
+        image: eattyfilly,
+        name: "EattyFilly",
+        description: "Online food ordering website where customers choose and order food from the available menu, which are then packaged and delivered speedily.",
+        language_one: "REACT",
+        language_two: "SASS",
     },
 ]
 
