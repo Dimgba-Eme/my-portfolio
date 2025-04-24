8404b0986c8b14ee039c693706612c53f880cb46
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-04-24 17:57:23 +0100

    Created the Footer component and imported it into the Home page

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
+import Footer from '../components/Footer'
 const Home = () => {
 
     useEffect(() => {
@@ -14,6 +15,7 @@ const Home = () => {
             <Hero />
             <Skills />
             <Projects />
+            <Footer />
         </main>
     )
 }
